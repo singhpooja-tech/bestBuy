9c5b0ef019fe9f37a18208be6622ad93e8ed4da8
singhpooja-tech <poojasingh3018@gmail.com>
2025-01-06 06:39:08 +0100

    store class to store product

---
+++ store.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+from products import Product
+
+
+class Store:
+    def __init__(self, products: List[Product]):
+        self.products = products
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+
+    def remove_product(self, product: Product):
+        if product in self.products:
+            self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        return sum(product.get_quantity() for product in self.products if product.is_active())
+
+    def get_all_products(self) -> List[Product]:
+        return [product for product in self.products if product.is_active()]
+
+    def order(self, shopping_list: List[Tuple[Product, int]]) -> float:
+        total_price = 0.0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
